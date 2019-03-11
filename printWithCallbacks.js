8c7a59dd40c68d07592edb0b28db0139855f54c4
Estella Song <estellajaysong@gmail.com>
2019-03-11 19:03:38 +0000

    step 4 completed, printing with callbacks

---
+++ printWithCallbacks.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getHTML (options, callback) 
+{ 
+  https.get(options, function (response) 
+  {
+    response.setEncoding('utf8');
+    response.on('data', function (data) 
+    { 
+      printHTML(data);
+    });
+  });
+};
+
+function printHTML (html) {
+    console.log(html);
+  }
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+  getHTML(requestOptions, printHTML())
