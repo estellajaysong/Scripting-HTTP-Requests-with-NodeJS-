b9783af35893c7eab0dfbed47e72727377998df3
Estella Song <estellajaysong@gmail.com>
2019-03-11 18:48:00 +0000

    completed step 3, printing with parameters

--- printWithParameters.js
+++ printWithParameters.js
@@ -1,13 +1,8 @@
 var https = require('https');
-var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step3.html'
-  };
-
 function getAndPrintHTML(options)
 {
   var bufferedData = '';
-  https.get(requestOptions, function (response) {
+  https.get(options, function (response) {
     response.setEncoding('utf8');
     response.on('data', function (data) {
       bufferedData += data;
@@ -18,5 +13,8 @@ function getAndPrintHTML(options)
     });
   });
 }
-
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
 getAndPrintHTML(requestOptions)
